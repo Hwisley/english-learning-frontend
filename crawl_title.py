dd4b782344b67609ec0489b05bddd92abe4982fe
hwisley0619 <hwisley0619@gmail.com>
2025-02-10 18:06:40 +0900

    Feat: implemented crawling title, eng and kor article

--- crawl_title.py
+++ crawl_title.py
@@ -1,14 +1,33 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome import options
 
-url = "https://cnn.ybmnet.co.kr/cnn_class/3364"
-# headers = {"User-Agent": "Mozilla/5.0"}
+options.page_load_strategy = 'normal'
 
-# response = requests.get(url, headers=headers)
-response = requests.get(url)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install(), options=options))
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, "html.parser", from_encoding='utf-8')
-    print(soup.prettify())  # HTML 확인
-else:
-    print("Failed to fetch page:", response.status_code)
+url="https://cnn.ybmnet.co.kr/cnn_class/3364"
+driver.implicitly_wait(1000)
+
+driver.get(url)
+
+driver.find_element(By.CLASS_NAME, "print").click()
+
+pages = driver.find_elements(By.CLASS_NAME, "page")
+innder_desktops = driver.find_elements(By.CLASS_NAME ,"inner.desktop")
+# import time
+# time.sleep(0.3)
+
+title = pages[1].find_element(By.CLASS_NAME, "title").text
+script_eng = pages[1].find_element(By.CLASS_NAME, "script").text
+# time.sleep(0.3)
+driver.find_element(By.CLASS_NAME, "cancel").click()
+script_kor = driver.find_element(By.CLASS_NAME, "translation").find_element(By.CLASS_NAME, "script").text
+
+print(title)
+print(script_eng)
+print(script_kor)
+
+
