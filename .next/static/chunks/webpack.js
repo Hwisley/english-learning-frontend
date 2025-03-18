cd8c838ad7d90a5e76cab8137250b908eac21df2
hwisley0619 <hwisley0619@gmail.com>
2025-03-18 14:04:23 +0900

    WIP: Change IDE to Cursor Agent + Claude 3.7
    requested to make up pages for English writing, and It worked..!

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b92d87cc5f221fcc"; }
+/******/ 		__webpack_require__.h = function() { return "27e1c9c3c094f00c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
